29dc68913f56b7e46cf0ce0e6d4e80cd596f9212
Fedenko Maksym <feden2906@gmail.com>
2021-03-10 00:57:19 +0200

    add setInterval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timer';
+
+  flag = false;
+  hour = 23;
+  min = 50;
+  sec = 0;
+
+  setIntervalFunction(): void {
+
+    const interval = setInterval(() => {
+        if (this.sec >= 59) {
+          this.sec = 0;
+
+          if (this.min >= 59) {
+            this.min = 0;
+            this.sec = 0;
+
+            if (this.hour >= 23) {
+              this.hour = 0;
+              this.min = 0;
+              this.sec = 0;
+              return;
+            }
+
+            this.hour += 1;
+            return;
+          }
+
+          this.min += 1;
+          return;
+        }
+
+        this.sec += 1;
+      }, 1000);
+    if (this.flag) {
+      clearInterval(interval);
+    }
+  }
+
+  reset(): void {
+    this.hour = 0;
+    this.min = 0;
+    this.sec = 0;
+  }
+
+  start(): void {
+    this.setIntervalFunction();
+    this.flag = true;
+  }
+
+  stop(): void {
+    this.reset();
+    this.setIntervalFunction();
+    this.flag = false;
+  }
+
+  wait(): void {
+
+    this.flag = false;
+  }
+
+
 }
+
